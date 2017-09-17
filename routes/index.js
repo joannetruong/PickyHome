63290452076dc1eea4517c635bd4e940f18f43f9
mdang8 <mdang808@gmail.com>
2017-09-17 02:14:44 -0400

    A lot of changes and routing stuff

---
+++ routes/index.js
@@ -0,0 +1,93 @@
+'use strict';
+
+let routes = require('express').Router();
+
+// GET request to home page
+routes.get('/', function (req, res) {
+    res.status(200).sendFile('index.html', {root: './'});
+});
+
+routes.get('/index.html', function (req, res) {
+    res.status(200).sendFile('index.html', {root: './'});
+});
+
+routes.get('/favicon.ico', function (req, res) {
+    res.status(200).send();
+});
+
+routes.get('/public/monitor.html', function (req, res) {
+    res.status(200).sendFile('monitor.html', {root: './public'});
+});
+
+routes.get('/public/control.html', function (req, res) {
+    res.status(200).sendFile('control.html', {root: './public'});
+});
+
+routes.get('/public/style.css', function (req, res) {
+    res.status(200).sendFile('style.css', {root: './public'});
+});
+
+routes.get('/public/index.js', function (req, res) {
+    res.status(200).sendFile('index.js', {root: './public'});
+});
+
+routes.get('/public/img/lamp1.jpg', function (req, res) {
+    res.status(200).sendFile('lamp1.jpg', {root: './public/img'});
+});
+
+routes.get('/public/img/light1.png', function (req, res) {
+    res.status(200).sendFile('light1.png', {root: './public/img'});
+});
+
+routes.get('/public/img/power.jpeg', function (req, res) {
+    res.status(200).sendFile('power.jpeg', {root: './public/img'});
+});
+
+routes.get('/public/img/power.png', function (req, res) {
+    res.status(200).sendFile('power.png', {root: './public/img'});
+});
+
+routes.get('/public/img/temp.png', function (req, res) {
+    res.status(200).sendFile('temp.png', {root: './public/img'});
+});
+
+routes.post('/command', function (req, res) {
+
+});
+
+routes.get('/readings', function (req, res) {
+    console.log('GET request to /readings.');
+    if (req.query) {
+        let type = req.query.type;
+        database.readData(type, function (data) {
+            res.status(200).send(data);
+        });
+    } else {
+        console.error('Request has no query string params.');
+        res.status(400).send('ERROR! There\'s no query string params! What is you doing???');
+    }
+});
+
+routes.post('/readings', function (req, res) {
+    console.log('POST request to /readings.');
+    if (req.body) {
+        console.log(req.body);
+        if (req.body.temp && req.body.energy) {
+            let data = {
+                temp: parseFloat(req.body.temp),
+                energy: parseFloat(req.body.energy)
+            };
+
+            database.writeData(data, function () {
+                res.status(200).send();
+            });
+        } else {
+            res.status(400).send('You fucked up.');
+        }
+    } else {
+        console.error('Request has no body.');
+        res.status(400).send('ERROR! There\'s no body in the request! What is you doing???');
+    }
+});
+
+module.exports = routes;
