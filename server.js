9226fed83d5ba81c7376781c12ed93d37a377c89
mdang8 <mdang808@gmail.com>
2017-09-16 22:47:48 -0400

    Server handles GET and POST requests for data readings

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let command = require(__dirname + '/lib/command.js');
+let database = require(__dirname + '/lib/database.js');
 let express = require('express');
 let bodyParser = require('body-parser');
 
@@ -17,4 +19,46 @@ app.listen(app.get('port'), function () {
 // GET request to home page
 app.get('/', function (req, res) {
     res.status(200).send('Hello World!');
+});
+
+app.get('/favicon.ico', function (req, res) {
+    res.sendStatus(200);
+});
+
+app.post('/command', function (req, res) {
+
+});
+
+app.get('/readings', function (req, res) {
+    if (req.query) {
+        let type = req.query.type;
+        database.readData(type, function (data) {
+            res.sendStatus(200).send(data);
+        });
+    } else {
+        console.error('Request has no query string params.');
+        res.sendStatus(400).send('ERROR! There\'s no query string params! What is you doing???');
+    }
+});
+
+app.post('/readings', function (req, res) {
+    console.log('Received.');
+    if (req.body) {
+        console.log(req.body);
+        if (req.body.temp && req.body.energy) {
+            let data = {
+                temp: parseFloat(req.body.temp),
+                energy: parseFloat(req.body.energy)
+            };
+
+            database.writeData(data, function () {
+                res.sendStatus(200);
+            });
+        } else {
+            res.sendStatus(400).send('You fucked up.');
+        }
+    } else {
+        console.error('Request has no body.');
+        res.sendStatus(400).send('ERROR! There\'s no body in the request! What is you doing???');
+    }
 });
